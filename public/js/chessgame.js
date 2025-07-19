14fbaec86309c76c0f057bf962fe6d05ce9f37cf
saksham087 <86426443+saksham087@users.noreply.github.com>
2025-07-20 00:53:55 +0530

    Update chessgame.js
    logic backend of moves in chess

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,45 @@
 // Client-side socket.io connection
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
 
+let draggedPiece = null;
+let sourcesquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach(( row , rowindex) => {});
+  row.forEach((square, squareindex) => {});
+  const squareElement = document.createElement("div");
+  squareElement.classList.add("square", 
+    ( rowindex +  squareindex)%2 === 0 ? "light" : "dark"
+  )
+
+    squareElement.dataset.row = rowindex;
+    squareElement.dataset.col = squareindex;
+
+    if (square) {
+        const pieceElement = document.createElement("div"); 
+        pieceElement.classList.add ("piece" , square.color === 'w' ? "white" : "black");
+        pieceElement.innerText = "";
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart", () => {
+            if(pieceElement.draggable) {
+                draggedPiece = pieceElement;
+                sourcesquare = { row: rowindex, col: squareindex }; 
+            }
+        });
+
+    }
+
+
+
+};
+
+const handlemove = () => { };
+const getPieceUnicode = () => {};
+
+renderBoard();
